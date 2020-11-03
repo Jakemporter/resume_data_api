4281f402ffb28096d2ea7a75f9e76fe92af1e0c6
Jonathan Wang <jdaegonw@gmail.com>
2020-11-02 20:35:52 -0600

    post api/educations

--- app/controllers/api/educations_controller.rb
+++ app/controllers/api/educations_controller.rb
@@ -1,12 +1,12 @@
 class Api::EducationsController < ApplicationController
   def create
-    @education = Education.new(
-      start_date: params[:start_date],
-      end_date: params[:end_date],
-      degree: params[:degree],
-      university_name: params[:university_name],
-      details: params[:details],
-    )
+      education = Education.new(
+        start_date: params[:start_date],
+        end_date: params[:end_date],
+        degree: params[:degree],
+        university_name: params[:university_name],
+        details: params[:details],
+      )
   if @education.save
     render json: { message: "Education created successfully" }, 
   else 
