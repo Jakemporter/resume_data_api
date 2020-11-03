83aad0ae2f7c56aff6a9ffb730effc22cf30c11d
Alan Barton <abarton201@gmail.com>
2020-11-02 20:50:02 -0500

    update index rought

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,15 +1,14 @@
 class Api::UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
-
     render "show.json.jb"
   end
 
   def index
-    @users = User.all  
+    @users = User.all
     render "index.json.jb"
-  end 
-  
+  end
+
   def create
     user = User.new(
       first_name: params[:first_name],
